039d9d2720bce2acdf6f7e3f529d1b05d51f7095
letsfckntry <melmathewzxc12@gmail.com>
2026-03-14 23:43:39 +0800

    zz

--- view/login.php
+++ view/login.php
@@ -217,7 +217,7 @@ window.onload = function() {
   <div class="relative w-full max-w-md">
     <!-- Logo -->
     <div class="absolute -top-12 left-1/2 transform -translate-x-1/2">
-      <img src="../assets/images/qcu-logo.png" alt="QCU Logo"
+      <img src="../assets/images/QCU-LOGO.png" alt="QCU Logo"
            class="w-20 h-20 rounded-full shadow-lg border-4 border-white dark:border-[#071127]">
     </div>
 
